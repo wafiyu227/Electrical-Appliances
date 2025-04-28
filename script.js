c3aad1c9bab9123f27a0f38f0ffe5f42e290711c
Ibrahim Wafiyudeen <ibrahimwafiyudeen@gmail.com>
2025-04-28 08:44:17 -1200

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+// Add click event listeners for each navigation item
+document.getElementById('home').addEventListener('click', () => {
+    window.location.href = 'index.html'; // Redirect to Home page
+});
+
+document.getElementById('about').addEventListener('click', () => {
+    window.location.href = 'about.html'; // Redirect to About page
+});
+
+document.getElementById('services').addEventListener('click', () => {
+    window.location.href = 'products.html'; // Redirect to Services page
+});
+
+document.getElementById('contact').addEventListener('click', () => {
+    window.location.href = 'contact.html'; // Redirect to Contact page
+});
